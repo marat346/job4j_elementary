7624908f960a0c9fa49ebc75271257d3d052057f
marat346 <dela346@mail.ru>
2021-11-18 15:30:02 +0300

    5.4. Протеиновая диета [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month += 1;
+            System.out.println(month);
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        Fitness.calc(95, 90);
+        Fitness.calc(90, 95);
+        Fitness.calc(50, 90);
+        Fitness.calc(90, 90);
+    }
+}
