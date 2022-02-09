07421614417d068cc69b59f67dadf8fa137f7715
marat346 <dela346@mail.ru>
2022-02-09 15:20:52 +0500

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -19,7 +19,7 @@ public class Point {
 
     public static void main(String[] args) {
         Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
+        Point b = new Point(5, 7);
         double dist = a.distance(b);
         System.out.println(dist);
     }
